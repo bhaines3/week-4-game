aa8dc2e96ccc1cf8be0e728f12d1ba66c9462fe3
Brandon Haines <haines.brandon.91@gmail.com>
2017-11-15 12:53:42 -0700

    working game update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,89 @@
 $(document).ready(function() {
     
-        crystals = ["assets/images/red-crystal.png","assets/images/orange-crystal.png","assets/images/purple-crystal.png","assets/images/pink-crystal.png"];
+            crystals = ["assets/images/red-crystal.png","assets/images/orange-crystal.png","assets/images/purple-crystal.png","assets/images/pink-crystal.png"];
     
-        var counter = 0;
-        var wins = 0;
-        var losses = 0;
-        $(".win").text(wins);
-        $(".loss").text(losses);
-        
-        newCrystals();
-        newGame();
+            var counter = 0;
+            var wins = 0;
+            var losses = 0;
+            var targetNum;
+            $(".win").text(wins);
+            $(".loss").text(losses);
     
-        function newCrystals () {
-            var numbers = []
-                while(numbers.length < 4){
-                  var randomnumber = Math.ceil(Math.random()*12)
-                  var found = false;
-                  for (var i=0; i< numbers.length; i++){
-                    if (numbers[i] == randomnumber){
-                        found = true; break
+            newCrystals();
+            newGame();
+    
+            function newCrystals () {
+                $(".crystals").empty();
+                var numbers = []
+                    while(numbers.length < 4){
+                      var randomnumber = Math.ceil(Math.random()*12)
+                      var found = false;
+                      for (var i=0; i < numbers.length; i++){
+                        if (numbers[i] == randomnumber){
+                            found = true;
+                            break;
+                        }
+                      }
+                      if(!found){
+                        numbers[numbers.length]=randomnumber;
+                      }
                     }
-                  }
-                  if(!found)numbers[numbers.length]=randomnumber;
-                }
-            console.log(numbers);		
+                console.log(numbers);
     
-            for (i = 0; i < numbers.length; i++) {
-                var imageCrystal = $("<img>");
-                imageCrystal.attr("data-num", numbers[i]);
-                imageCrystal.attr("src", crystals[i]);
-                imageCrystal.attr("alt", "crystals");
-                imageCrystal.addClass("gem-img")
-                $(".crystals").append(imageCrystal);
+                for (i = 0; i < numbers.length; i++) {
+                    var divCol = $("<div class='col-3'>")
+                    var divRow = $("<div class='row text-center'>")
+                    var imageCrystal = $("<img>");
+                    imageCrystal.attr("data-num", numbers[i]);
+                    imageCrystal.attr("src", crystals[i]);
+                    imageCrystal.attr("alt", "crystals");
+                    imageCrystal.addClass("gem-img");
+                    divRow.append(imageCrystal)
+                    divCol.append(divRow)
+                    $(".crystals").append(divCol);
+                }
             }
-        }
-//play new game    
-        function newGame() {
+    //play new game
+            function newGame() {
     
-            counter = 0;
-            $(".user-score").text(counter);
+                counter = 0;
+                $(".user-score").text(counter);
     
-            function randomIntFromInterval(min,max){
-                   return Math.floor(Math.random()*(max-min+1)+min);
+                function randomIntFromInterval(min, max){
+                  return Math.floor(Math.random() * (max - min) + min);
                 }
     
-            var targetNum = randomIntFromInterval(19,120);
+                targetNum = randomIntFromInterval(19, 120);
     
-            $(".value").text(targetNum);
+                $(".value").text(targetNum);
     
+            }
     
-            $(".gem-img").on("click", function(){
-                counter = counter + parseInt($(this).data("num"));
-               
-                $(".yourScore").text(counter);
+            //document.addEventListener("click", function)
     
-                if (counter == numberToGuess){
-                  $(".status").text("You won!!!!");
-                  wins ++;
-                  $(".win").text(wins);
-                  console.log(wins)
-                  $(".crystals").empty();
-                  newCrystals();
-                  newGame();
-                    
-                } else if ( counter > targetNum){
-                    $(".status").text("You lost!")
-                    losses ++;
-                    $(".loss").text(losses);
-                    console.log(losses)
-                    $(".crystals").empty();
-                    newCrystals();
-                    newGame();
-                }
-            });
-        }
+            $(".crystals").on("click", ".gem-img", function(){
+              console.log("this is working");
+              counter += parseInt($(this).attr("data-num"));
     
+              $("#yourScore").text(counter);
+    
+              if (counter == targetNum){
+                $(".status").text("You won!!!!");
+                ++wins;
+                $(".wins").text(wins);
+                console.log(wins)
+                newCrystals();
+                newGame();
+    
+              } else if ( counter > targetNum){
+                $(".status").text("You lost!")
+                ++losses;
+                $(".losses").text(losses);
+                console.log(losses)
+                $(".crystals").empty();
+                newCrystals();
+                newGame();
+              }
+        });
 });
+    
